f4d3f7e9c77f5da759b10cbca059aa39e324f3ef
Nate Stephany <nate@redhat.com>
2016-06-16 15:24:47 -0700

    Update cloud_orchestration_preprovision.rb

--- automate/methods/cloud_orchestration_preprovision.rb
+++ automate/methods/cloud_orchestration_preprovision.rb
@@ -1,31 +1,49 @@
-#
-# Description: This method prepares arguments and parameters for orchestration provisioning
-#
+=begin
+  preprovision.rb
+
+  Author: Nate Stephany <nate@redhat.com>
+
+  Description: This is an extended preprovision method for deploying OpenStack
+  			   Heat stacks that rely on nested templates. It will read those
+  			   nested templates out of CloudForms and feed them in as a string.
+
+  Mandatory dialog fields: dialog_nested_template_XXX where XXX is the name of
+  						   your nested template yaml file (i.e. worker.yaml)
+-------------------------------------------------------------------------------
+   Copyright 2016 Nate Stephany <nate@redhat.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-------------------------------------------------------------------------------
+=end
 def log(level, msg, update_message = false)
   $evm.log(level, "#{msg}")
   @task.message = msg if @task && (update_message || level == 'error')
 end
 
-$evm.log("info", "Starting Orchestration Pre-Provisioning")
+#
+# TODO: test and extend this to make sure it works with other provider orchestration
+#
+
+log("info", "Starting Orchestration Pre-Provisioning")
 
 service = $evm.root["service_template_provision_task"].destination
 
-# Through service you can examine the orchestration template, manager (i.e., provider)
-# stack_name, and options to create the stack
-# You can also override these selections through service
+log("info", "manager = #{service.orchestration_manager.name}(#{service.orchestration_manager.id})")
+log("info", "template = #{service.orchestration_template.name}(#{service.orchestration_template.id}))")
+log("info", "stack name = #{service.stack_name}")
 
-$evm.log("info", "manager = #{service.orchestration_manager.name}(#{service.orchestration_manager.id})")
-$evm.log("info", "template = #{service.orchestration_template.name}(#{service.orchestration_template.id}))")
-$evm.log("info", "stack name = #{service.stack_name}")
-# Caution: stack_options may contain passwords.
-# $evm.log("info", "stack options = #{service.stack_options.inspect}")
-
-# Example how to programmatically modify stack options:
-# service.stack_name = 'new_name'
+service.name = service.stack_name
 stack_options = service.stack_options
-# stack_options[:disable_rollback] = false
-# stack_options[:timeout_mins] = 2 # this option is provider dependent
-# stack_options[:parameters]['flavor'] = 'm1.small'
 
 $evm.root.attributes.sort.each { |k, v| log(:info, "\t $evm.root Attribute - #{k}: #{v}")}
 nested_templates = {}
@@ -40,6 +58,7 @@ log(:info, "stripped prefixes from keys in nested_templates: #{nested_templates.
 nested_templates.clone.each { |k,v| nested_templates[k.sub(/$/, '.yaml')] = nested_templates.delete(k) }
 log(:info, "appended .yaml to nested template name to make it all work: #{nested_templates.inspect}")
 
-stack_options[:files] = nested_templates
-# # Important: set stack_options
-service.stack_options = stack_options
+unless nested_templates.blank?
+  stack_options[:files] = nested_templates
+  service.stack_options = stack_options
+end
