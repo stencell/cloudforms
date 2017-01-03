365cf367219c5c8a84d1aaf7a129d8db9ac82def
Nate Stephany <nstephan@nstephan-tp.usersys.redhat.com>
2017-01-03 14:19:40 -0800

    added misc automate methods

--- automate/methods/cloud_orchestration_preprovision.rb
+++ automate/methods/cloud_orchestration_preprovision.rb
@@ -6,7 +6,7 @@ def log(level, msg, update_message = false)
   @task.message = msg if @task && (update_message || level == 'error')
 end
 
-$evm.log("info", "Starting Orchestration Pre-Provisioning")
+log("info", "Starting Orchestration Pre-Provisioning")
 
 service = $evm.root["service_template_provision_task"].destination
 
@@ -14,14 +14,15 @@ service = $evm.root["service_template_provision_task"].destination
 # stack_name, and options to create the stack
 # You can also override these selections through service
 
-$evm.log("info", "manager = #{service.orchestration_manager.name}(#{service.orchestration_manager.id})")
-$evm.log("info", "template = #{service.orchestration_template.name}(#{service.orchestration_template.id}))")
-$evm.log("info", "stack name = #{service.stack_name}")
+log("info", "manager = #{service.orchestration_manager.name}(#{service.orchestration_manager.id})")
+log("info", "template = #{service.orchestration_template.name}(#{service.orchestration_template.id}))")
+log("info", "stack name = #{service.stack_name}")
 # Caution: stack_options may contain passwords.
 # $evm.log("info", "stack options = #{service.stack_options.inspect}")
 
 # Example how to programmatically modify stack options:
 # service.stack_name = 'new_name'
+service.name = service.stack_name
 stack_options = service.stack_options
 # stack_options[:disable_rollback] = false
 # stack_options[:timeout_mins] = 2 # this option is provider dependent
@@ -40,6 +41,8 @@ log(:info, "stripped prefixes from keys in nested_templates: #{nested_templates.
 nested_templates.clone.each { |k,v| nested_templates[k.sub(/$/, '.yaml')] = nested_templates.delete(k) }
 log(:info, "appended .yaml to nested template name to make it all work: #{nested_templates.inspect}")
 
-stack_options[:files] = nested_templates
-# # Important: set stack_options
-service.stack_options = stack_options
+unless nested_templates.blank?
+  stack_options[:files] = nested_templates
+  # # Important: set stack_options
+  service.stack_options = stack_options
+end
